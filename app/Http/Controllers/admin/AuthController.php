d952783da7dd69caecd500804affc018740b9165
aqashyy <ashiqmuhammedep@gmail.com>
2025-05-16 15:22:15 +0530

    ecommerce react backend started and admin auth controller created and its login functions

---
+++ app/Http/Controllers/admin/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function authenticate(Request $request) {
+        $validator  =   Validator::make($request->all(),[
+            'email'     =>  'required|email',
+            'password'  =>      'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'    =>  400,
+                'errors'    =>  $validator->errors()
+            ],400);
+        }
+        if(Auth::attempt(['email'   =>  $request->email, 'password'  =>  $request->password])){
+            $user   =   User::find(Auth::user()->id);
+            if($user->role == 'admin'){
+
+            }else{
+                return response()->json([
+                    'status'    =>  401,
+                    'message'   =>  'you are not authorize to access admin panel!'
+                ],401);
+            }
+        }else{
+            return response()->json([
+                'status'    =>  401,
+                'message'   =>  'Invalid email/password!'
+            ],401);
+        }
+    }
+}
