a3927942e87eeb46267c5a106c411cf33563e00d
aqashyy <ashiqmuhammedep@gmail.com>
2025-06-25 15:33:45 +0530

    product image gallery upload function has been created

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\TempImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class ProductController extends Controller
 {
@@ -50,11 +53,39 @@ class ProductController extends Controller
         $product->qty               =   $request->qty;
         $product->description       =   $request->description;
         $product->short_description =   $request->short_description;
-        $product->image             =   $request->image;
         $product->barcode           =   $request->barcode;
         $product->status            =   $request->status;
         $product->is_featured       =   $request->is_featured;
         $product->save();
+
+        // Save product images
+
+        if(!empty($request->gallery)) {
+            foreach($request->gallery as $key =>  $tempImageId) {
+                $tempImage  =   TempImage::find($tempImageId);
+
+                // Large thumbnail
+                $extArray   =   explode('.',$tempImage->name);
+                $ext        =   end($extArray);
+
+                $imageName  =   $product->id.'-'. time() .'.'.$ext;
+                $manager = new ImageManager(Driver::class);
+                $img        =   $manager->read(public_path('uploads/temp/'.$tempImage->name));
+                $img->scaleDown(1200);
+                $img->save(public_path('uploads/products/large/'.$imageName));
+
+                // Small Thumbnail
+                $manager = new ImageManager(Driver::class);
+                $img        =   $manager->read(public_path('uploads/temp/'.$tempImage->name));
+                $img->coverDown(400,460);
+                $img->save(public_path('uploads/products/small/'.$imageName));
+
+                if($key == 0) {
+                    $product->image     =   $imageName;
+                    $product->save();
+                }
+            }
+        }
 
         return response()->json([
             'status'    => 200,
@@ -139,7 +170,7 @@ class ProductController extends Controller
                 'message'   =>  "Product not found!"
             ], 404);
         }
-        
+
         $product->delete();
 
         return response()->json([
